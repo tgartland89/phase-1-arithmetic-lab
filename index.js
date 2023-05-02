66ab7450494864f6180e96552918aa888cf3125f
tgartland89 <thomas.gartland@gmail.com>
2023-05-01 21:23:44 -0600

    arithmateic variables

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+var num1 = 31
+var num2 = 2
+var num3 = 20
+var num4 = 5
+var multiply = num1 * num2
+
+var random = Number.isInteger(random)
+random = 1
+
+var mod = num3/num4
+
+var max = Math.max (1,2,3,20)
